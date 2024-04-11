85ce62a49fb148afb2b5460f15c5e493fb362413
ari117 <wenrui117@gmail.com>
2024-04-11 16:22:07 +0800

    make it better

--- app.py
+++ app.py
@@ -29,6 +29,12 @@ from linebot.models import (
     CarouselTemplate,
     CarouselColumn
 )
+import tempfile, os
+import datetime
+import openai
+import time
+import traceback
+
 from modules.reply import faq,menu
 from modules.currency import get_exchange_table
 from modules.app2023 import get_exchange_table2
@@ -44,15 +50,28 @@ table3=get_exchange_table3()
 # 定義應用程式是一個Flask類別產生的實例
 
 app = Flask(__name__)
-
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # LINE的Webhook為了辨識開發者身份所需的資料
 # 相關訊息進入網址(https://developers.line.me/console/)
-CHANNEL_ACCESS_TOKEN = 'B9PSwb+aYbK5UiFplp47lNzGm+UPMTKSGdar02wWfQZkXyF3jM7DHEOxM7bzq8SjEn83AA8iJxw59pojw5EBa2w6ELQNwenSD0QvCKXF7WXwUoObGCj3u8cwdTItnDWj1ugk8dmYjBqeV7guah58agdB04t89/1O/w1cDnyilFU='
-CHANNEL_SECRET = '207d15b8d40fa0c6d18cb8f20d717352'
+#CHANNEL_ACCESS_TOKEN = 'B9PSwb+aYbK5UiFplp47lNzGm+UPMTKSGdar02wWfQZkXyF3jM7DHEOxM7bzq8SjEn83AA8iJxw59pojw5EBa2w6ELQNwenSD0QvCKXF7WXwUoObGCj3u8cwdTItnDWj1ugk8dmYjBqeV7guah58agdB04t89/1O/w1cDnyilFU='
+#CHANNEL_SECRET = '207d15b8d40fa0c6d18cb8f20d717352'
 
 # ********* 以下為 X-LINE-SIGNATURE 驗證程序 *********
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(CHANNEL_SECRET)
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def GPT_response(text):
+    # 接收回應
+    response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=text, temperature=0.5, max_tokens=500)
+    print(response)
+    # 重組回應
+    answer = response['choices'][0]['text'].replace('。','')
+    return answer
+
+
+
 @app.route("/", methods=['POST'])
 def callback():
     # 當LINE發送訊息給機器人時，從header取得 X-Line-Signature
@@ -61,6 +80,7 @@ def callback():
 
     # 將取得到的body內容轉換為文字處理
     body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
     print("[BODY]")
     print(body)
 
@@ -111,6 +131,32 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         reply)
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    try:
+        GPT_answer = GPT_response(msg)
+        print(GPT_answer)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    except:
+        print(traceback.format_exc())
+        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
+
+
+@handler.add(PostbackEvent)
+def handle_message(event):
+    print(event.postback.data)
+
+
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入')
+    line_bot_api.reply_message(event.reply_token, message)
 
 
 # 貼圖訊息傳入時的處理器 
